0b8f7cd187d6314ba75f3017887fcd0f2e75bd1b
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-04-15 18:57:41 +0300

    add hot reload + type arguments for function

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const name = "Maks"
+const age = 32
+const gender = "male"
+
+const sayHi = (name: string, age: number, gender: string): void => console.log(`Hi ${name} - ${age} - ${gender}`)
+
+sayHi(name, age, gender)
+
+export {}
