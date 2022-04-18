6a8c21b3c448d85f7649d17ce2ccb32072bd1507
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-04-18 08:41:49 +0300

    change interface to class

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,22 @@ const name = "Maks"
 const age = 32
 const gender = "male"
 
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string
+    public age: number
+    public gender: string
+
+    constructor(name: string, age: number, gender: string) {
+        this.name = name
+        this.age = age
+        this.gender = gender
+    }
 }
 
-const person = {
-    name,
-    age,
-    gender,
-}
+const anton = new Human(name, age, gender)
 
 const sayHi = (person: Human): void => console.log(`Hi ${person.name} - ${person.age} - ${person.gender}`)
 
-sayHi(person)
+sayHi(anton)
 
 export {}
